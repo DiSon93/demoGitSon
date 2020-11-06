034a764b46a901602201ea114d8e8a2a4e125a56
DiSon93 <sonnguyendi93@gmail.com>
2020-11-06 20:41:03 +0700

    tạo mơi

--- demo.js
+++ demo.js
@@ -2,3 +2,11 @@ console.log("Khởi tạo dự án");
 console.log("Code thay đổi");
 console.log("Thử xem nào!");
 console.log("Code thay đổi");
+
+// git add -A (đẩy xái mới nhất) .(đẩy toàn bộ)
+//git commit -m "đẩy lên git" (lưu vô 1 file)
+// git push (đẩy code mới lên)
+
+// git pull (lấy về)
+
+//git clone (lấy link dự án mới)
